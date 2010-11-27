050550c8495fc287f7e7c3193135dc20368a614a
jessykate <jessy.cowansharp@gmail.com>
2010-11-27 18:59:45 -0500

    more verbose test printing for debugging

--- tests.rb
+++ tests.rb
@@ -2,7 +2,7 @@ require 'uds.rb'
 require 'lms.rb'
 # require 'pubsub.rb'
 
-# Running some lame tests :P
+# Test the UDS layer
 uds = UDS.new(5, 100, 100, 15, "euclidean")
 n1 = uds.add(50, 50)
 n2 = uds.add(50, 40)
@@ -22,40 +22,58 @@ uds.remove(n3)
 uds.remove_all()
 uds.print_nodes # should print 'no nodes'
 
-# Play around with all of these numbers to see the difference... is crazy :p but it kinda makes sense
-uds = UDS.new(100, 300, 300, 50, "euclidean") #gotta initialize uds again since we just removed all the nodes :p
 
+# Set up the physical network
+# Play around with all of these numbers to see the difference... is crazy :p
+# but it kinda makes sense
+# initialize uds again since we just removed all the nodes :p
+num_nodes = 40
+width = 100
+height = 100
+nbr_dist = 50
+distance_metric = 'euclidean'
+uds = UDS.new(num_nodes, width, height, nbr_dist, distance_metric) 
 
-hops = 5  # CHANGE THIS AND SEE THE DIFFERENCE :P
+# Set up the LMS routing layer
+hops = 2  # CHANGE THIS AND SEE THE DIFFERENCE :P
 lambda_ = 256
 network = uds
-max_buffer = 10
 max_failures = 5
-lms = LMS.new(hops, lambda_, uds, max_buffer, max_failures)
+lms = LMS.new(hops, lambda_, uds, max_failures)
 
+# define some messages 
 nature_msg = "punk rock kittens"
 event_msg = "chinese festival of dragons"
 review_msg = "the chicken today is terrible"
 
-# the key of the put request is like the tag. 
-
-# XXX TODO need to pull out the LMS node IDs so we can choose IDs for the (EASY..... just call uds.nodes() :P)
-# 'initiator' argumentof put/get
-
+# pull out the LMS node IDs so we can choose IDs for the 'initiator' argument
+# of put/get
 ids = uds.nodes
-first=  ids.keys[0]
-second = ids.keys[1]
+n1 =  ids.keys[0]
+n2 = ids.keys[num_nodes/2]
 
-
-
-lms.put(first, nature_msg, 'nature', 10)
-lms.put(first, event_msg, 'event', 10)
-lms.put(second, review_msg, 'review', 10)  #notice that I'm inserting it with node 2nd
+lms.put(n1, nature_msg, 'nature', 10)
+lms.put(n1, event_msg, 'event', 10)
+lms.put(n2, review_msg, 'review', 10)  #notice that I'm inserting it with node 2nd
 
 puts " ---- GETS START HERE ----"
-puts lms.get(first, 'event')
-puts lms.get(second, 'event')
-puts lms.get(first, 'review')
-puts lms.get(second, 'review')
-puts lms.get(first, 'nature')
-puts lms.get(second, 'nature')
+puts "Node #{n1} retrieving items with tag 'event'" 
+node, probe = lms.get(n1, 'event')
+p "Probe returned local minimum #{node.id} with path #{probe.getPath}"
+
+puts "Node #{n2} retrieving items with tag 'event'" 
+node, probe = lms.get(n2, 'event')
+p "Probe returned local minimum #{node.id} with path #{probe.getPath}"
+
+puts "Node #{n1} retrieving items with tag 'review'" 
+puts lms.get(n1, 'review')
+
+puts "Node #{n2} retrieving items with tag 'review'" 
+puts lms.get(n2, 'review')
+
+puts "Node #{n1} retrieving items with tag 'nature'" 
+puts lms.get(n1, 'nature')
+
+puts "Node #{n1} retrieving items with tag 'nature'" 
+puts lms.get(n2, 'nature')
+
