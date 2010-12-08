28918d36bb9d958f48a149f3a27207a01bccd037
sga001 <sga001@gmail.com>
2010-12-08 03:02:51 -0500

    updated

--- tests2.rb
+++ tests2.rb
@@ -2,13 +2,13 @@ require 'fogpubsub.rb'
 require 'uds.rb'
 require 'lms.rb'
 
-
-topology = UDS.new(width=400, height=400, nbr_dist=50)
+topology = UDS.new(400, 400, 50)
 universe = God.new(topology)
 
 tags = ["events", "system", "reviews", "happy", "blah", "animals"]
 lambda_ = 256
 hops = 2
+num_nodes = 10
 
 
 
@@ -16,8 +16,7 @@ hops = 2
   x = rand(width)
   y = rand(height)
   subscriptions = tags[rand(tags.length)] 
-  universe.add(FogNode.new(id, x, y, routing=LMS, lambda_, hops, 
-                buffer_size = rand(20), max_failures = 5))
+  universe.add(FogNode.new(id, routing=LMS, lambda_, hops, buffer_size = rand(20), max_failures = 5, x, y))
 }
 
 
