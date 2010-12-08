c0a74ab6bbc50cb44469200458ad914a9a8f7442
sga001 <sga001@gmail.com>
2010-12-08 03:03:07 -0500

    Updated but it has a MAJOR design flaw that we should discuss!

--- fogpubsub.rb
+++ fogpubsub.rb
@@ -8,17 +8,17 @@ A publish/subscribe mechanism. Runs on each node in the network.
 
 
 class FogNode
-  def initialize(nid, x=nil, y=nil, subscriptions=nil, routing, lambda_, 
-                 hops, buffer_size, max_failures)
+  def initialize(nid, routing, lambda_, hops, buffer_size, max_failures, god, x, y, subscriptions=nil)
     @nid = nid
-    @x = x 
+    @x = x
     @y = y
-    @subscriptions = subscriptions
+    @subscriptions = subscriptions || []
     @max_buffer = buffer_size ||= rand(20)
     @buffer = {}
+    @digest = BloomFilter.new(512, 10) # 512 bits, 10 hash functions
+    @god = god
     # expects routing interface to implement put, get
-    @routing = routing(@nid, hops, lambda_, @buffer, @max_buffer, max_failures) 
-    @neighbors = nil
+    @routing = routing.new(@nid, hops, lambda_, max_failures, @god) 
     # initialize neighbor list
     @routing.neighbours_update() 
   end
@@ -37,9 +37,11 @@ class FogNode
   def store
   end
 
-  def publish(nid, item, tag, expiry, radius, replicas)
+  def publish(item, tag, expiry, radius, replicas)
     # currently just calls the PUT method of the routing/storage layer
-    @routing.put(nid, item, tag, replicas)
+    key = Digest::SHA1.hexdigest(item.to_s) #this makes it a unique key
+    value = FogDataObject.new(item, tag, expiry, radius)
+    @routing.put(key, value, replicas)
   end
 
   def subscribe(nid, tag)
@@ -90,8 +92,69 @@ class FogNode
     return "[" + @x.to_s + "," + @y.to_s + "]"
   end
 
+  # --------------------------------------------------
+  # Physical Storage Methods
+  # --------------------------------------------------
 
+   
+   def bufferAdd(k, item)
+    unless bufferFull?
+      @buffer.store(k, item)
+      @digest.insert(k)
+    else
+      raise "Max buffer size exceeded"
+    end
+  end
+  
+  def contains?(k)
+    return @buffer.key?(k)
+  end
 
+  def bufferFull?
+    if @buffer.length >= @max_buffer
+      return true
+    else
+      return false
+    end
+  end
+
+  def bufferLength()
+    return @buffer.length
+  end
+  
+  def retrieve(k)
+    return @buffer[k]
+  end
+
+  def digest()
+    return @digest
+  end
+
+end
+
+=begin
+  This class represents a Fog Data Object which is composed of: Tag, Message, Expire Date and Radius
+=end
+class FogDataObject
+  def initialize(tag, message, expiry, radius)
+    @tag, @message, @expiry, @radius = tag, message, expiry, radius
+  end
+  
+  def tag
+    return @tag
+  end
+  
+  def message
+    return @message
+  end
+  
+  def expiry
+    return @expiry
+  end
+  
+  def radius
+    return @radius
+  end
 end
 
 
