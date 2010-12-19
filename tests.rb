588ad713a7e6ad3a94f122a0bd5ec149f2a7352b
jessykate <jessy.cowansharp@gmail.com>
2010-12-19 02:32:05 -0500

    changing tests to focus on LMS, get some aggregate peformace stats

--- tests.rb
+++ tests.rb
@@ -1,53 +1,100 @@
+#!/usr/bin/ruby
+
 require 'fogpubsub.rb'
 require 'uds.rb'
 require 'lms.rb'
 require 'pp'
-=begin
-Time to do complicated tests involving: MOVING AROUND, WEIRD TOPOLOGIES, ETC.
-I also did not implement the TIME DISCRIMINATOR STUFF and therefore expiry doesn't work.
-=end
 
-width = 200
-height = 200
-topology = UDS.new(width, height, 20)
+# -------------------------------------------------
+#           LMS layer experiments
+# -------------------------------------------------
+# GLOBAL PARAMS - do not change across experiments
+# -------------------------------------------------
 
-# some initial set of tags. this could be empty, and can be changed over time. 
-tags = ["events", "system", "reviews", "happy", "blah", "animals"]
-# parameter to determine length of hashIDs in LMS
+max_failures = 5
+
+# how many replicas to store. should be adaptive but isn't ;)
+replicas = 10
+
+# LMS hashID parameter
 lambda_ = 256
-# LMS parameter
-hops = 2
-# network parameter
-num_nodes = 800
 
-#universe = God.new(topology)
-universe = God.new(topology, hops)
+# think of these as meters. 
+world_width = 1000
+world_height = 1000
 
-(1..num_nodes).each{|id|
-  x = rand(width)
-  y = rand(height)
-  subscription = tags[rand(tags.length)] 
-  fog_node = FogNode.new(id, routing=LMS, lambda_=256, hops=2, buffer_size = rand(20), max_failures = 5, x, y, subscription)
-  universe.add(fog_node)
+# -------------------------------------------------
+# -------------------------------------------------
+
+# experiments where we vary the number of nodes, hops, and
+# braodcast radius.
+(1..2).each { |hops|
+    [100,200,300].each{ |broadcast_radius|
+        topology = UDS.new(world_width, world_height, broadcast_radius)
+        universe = God.new(topology)
+        (10..1000).step(10).each { |num_nodes| 
+
+            # initialize nodes in the universe with randomized subscriptions
+            # and a random buffer size between 1 and 20
+            (1..num_nodes).each{|id|
+                x = rand(world_width)
+                y = rand(world_height)
+                fog_node = FogNode.new(id, routing=LMS, lambda_, hops, 
+                            buffer_size = rand(20), max_failures, x, y)
+                universe.add(fog_node)
+            }
+
+            # pick off some nodes to demonstrate put and get operations with 
+            n1 = universe.getNode(1)
+            n2 = universe.getNode(2)
+            n3 = universe.getNode(3)
+
+            # place ten messages into the system. why 10? does one put
+            # operation interact with others? only if we are exploring backoff
+            # behaviour, but since we have the system set up we might as well
+            # put a bunch of messages out there. 
+            n1.routing.put(tag = "t1", message = "n1 publishes with tag t1", replicas)
+            n2.routing.put(tag = "t2", message = "n2 publishes with tag t2", replicas)
+            n3.routing.put(tag = "t3", message = "n3 publishes with tag t3", replicas)
+            n1.routing.put(tag = "t4", message = "n1 publishes with tag t4", replicas)
+            n2.routing.put(tag = "t5", message = "n2 publishes with tag t5", replicas)
+            n3.routing.put(tag = "t6", message = "n3 publishes with tag t6", replicas)
+            n1.routing.put(tag = "t7", message = "n1 publishes with tag t7", replicas)
+            n2.routing.put(tag = "t8", message = "n2 publishes with tag t8", replicas)
+            n3.routing.put(tag = "t9", message = "n3 publishes with tag t9", replicas)
+            n1.routing.put(tag = "t10", message = "n1 publishes with tag t10", replicas)
+
+            # get some new nodes and do some querying
+            n4 = universe.getNode(4)
+            n5 = universe.getNode(5)
+            n6 = universe.getNode(6)
+
+            # query for each of the items - 'managed' get will repeat the get
+            # request until it succeeds or some max threshold is hit. 
+            n4.routing.managedGet("t0") 
+            n5.routing.managedGet("t2")
+            n6.routing.managedGet("t3")
+            n4.routing.managedGet("t4") 
+            n5.routing.managedGet("t5")
+            n6.routing.managedGet("t6")
+            n4.routing.managedGet("t7") 
+            n5.routing.managedGet("t8")
+            n6.routing.managedGet("t9")
+            n4.routing.managedGet("t10") 
+            
+            exit!
+
+        }
+    }   
 }
 
-n1 = universe.getNode(1)
-n2 = universe.getNode(2)
-n3 = universe.getNode(3)
-newguy = FogNode.new(1000, routing=LMS, lambda_, hops, buffer_size = rand(20), max_failures = 5, x=75, y = 75)
-universe.add(newguy)
-universe.updateAllNeighbors()
+exit!
 
-n1.publish(tag = "haiku", message = "<--START-->", lifetime = 20, radius = 500, replicas = 10)
-n1.publish(tag = "haiku", message = "alice’s plaintext", lifetime = 20, radius = 500, replicas = 10)
-n2.publish(tag = "haiku", message = "proxy and delegatee", lifetime = 20, radius = 500, replicas = 10)
-n3.publish(tag = "haiku", message = "fear for collusion", lifetime = 20, radius = 500, replicas = 10)
-n3.publish(tag = "haiku", message = "<--END-->", lifetime = 20, radius = 90, replicas = 10)
-n2.publish(tag = "news", message = "I LOVE FOG", lifetime = 20, radius = 500, replicas = 10)
-n2.publish(tag = "news", message = "Poor bobby.. no alien life", lifetime = 20, radius = 500, replicas = 10)
-n2.publish(tag = "news", message = "I'm running out of things to say for test cases", lifetime = 20, radius = 500, replicas = 10)
-n2.publish(tag = "news", message = "life sucks", lifetime = 20, radius = 500, replicas = 10)
+# other things you can do
 
+=begin
+# newguy = FogNode.new(1000, routing=LMS, lambda_, hops, buffer_size = rand(20), max_failures = 5, x=75, y = 75)
+# universe.add(newguy)
 puts "\nN2"
 puts n2.query("haiku")
 puts "\nNEWGUY"
@@ -76,4 +123,4 @@ newguy.addSubscription("news")
 pp newguy.check()
 pp newguy.cached()
 
-
+=end
