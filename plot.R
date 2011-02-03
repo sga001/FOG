808fdb79574b87362bb6ef44d6d89308499919c9
jessykate <jessy.cowansharp@gmail.com>
2011-02-02 23:13:16 -0500

    changes to plot

--- plot.R
+++ plot.R
@@ -2,19 +2,19 @@
 # then type source("plot.R") at the R prompt. 
 
 # fill in the most recent logfile name here
-#log <- read.csv("log-2010-12-20T22:26:46-05:00")
-log <- read.csv("log-2010-12-21T08:43:44-05:00")
+log <- read.csv("log-2011-01-27T22:37:01-05:00")
 # start the PNG device driver to save the graph
-#png(filename="/home/jessy/umd/manet_pubsub/lms-recall2.png", height=400, width=600, bg="white")
+#png(filename="graphs/lms-recall2.png", height=400, width=600, bg="white")
 
-nodes <- log$nodes
+nodes <- log$broadcast
 put_recall <- log$put.recall
 get_recall <- log$get.recall
 colours = c("darkorange4", "darkorange2")
 plot(nodes, put_recall, type="o", ylim=range(0,1), ann=FALSE, col=colours[1])
-title(xlab="Number of Nodes", ylab="Recall", main="LMS Performance\nHops=1, Broadcast Radius=100, Width = Height = 1000")
+title(xlab="Broadcast Radius", ylab="Recall", main="LMS Performance\nHops=1, Broadcast Radius=100, Width = Height = 1000")
 lines(nodes, get_recall, type="o", lty=2, pch=22, col=colours[2])
-legend(100,1.0, legend=c("PUT Recall", "GET Recall"), border=FALSE, col=colours, lty=1:2, pch=21:22) 
+legend(100,1.0, legend=c("PUT Recall 2 Hops",
+                  "GET Recall 2 hops"), border=FALSE, col=colours, lty=1:2, pch=21:22) 
 
 # turn the device driver off (flushes the PNG output as well)
 #dev.off()
